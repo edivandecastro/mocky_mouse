a0eeb75b525f3cfff04db8a041e250732c2aad22
Edivan de Castro <edivandecastro@gmail.com>
2024-09-07 00:22:18 -0300

    Adicionado user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_08_13_144704) do
+ActiveRecord::Schema.define(version: 2024_09_07_031843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,19 @@ ActiveRecord::Schema.define(version: 2024_08_13_144704) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["mock_order"], name: "index_mocks_on_mock_order", unique: true
     t.index ["name"], name: "index_mocks_on_name", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "headers", "mocks", on_delete: :cascade
