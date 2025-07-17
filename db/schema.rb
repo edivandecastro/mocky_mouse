17dbc5a85838ef05a747a4f50ef8c4c9129b1ea2
Edivan de Castro <edivandecastro@gmail.com>
2025-07-17 19:31:07 -0300

    Add migration for projects table with user reference and timestamps

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_09_08_131441) do
+ActiveRecord::Schema.define(version: 2025_07_17_210201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,15 @@ ActiveRecord::Schema.define(version: 2024_09_08_131441) do
     t.index ["user_id"], name: "index_mocks_on_user_id"
   end
 
+  create_table "projects", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
@@ -68,4 +77,5 @@ ActiveRecord::Schema.define(version: 2024_09_08_131441) do
 
   add_foreign_key "headers", "mocks", on_delete: :cascade
   add_foreign_key "mocks", "users"
+  add_foreign_key "projects", "users"
 end
